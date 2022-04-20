1f4f46e0e625864fe85bbd279f38588bc39838c7
Muzamilkhan-Waveit <muzamilkhan111814@gmail.com>
2022-04-21 00:23:42 +0500

    Final Commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -64,3 +64,100 @@ class constants2 {
     color: Color.fromARGB(255, 110, 100, 4),
   );
 }
+
+
+ isDark ? darkBgColor : lightBgColor
+style: isDark ? dark.heading1 : light.heading1
+isDark ? dark.iconColor : light.iconColor,
+isDark
+                                        ? dark.borderColor
+                                        : light.borderColor,
+
+  if (isUrdu == true) {
+    } else {}
+
+
+Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(50),
+                          color: isDark
+                              ? dark.iconBackground
+                              : light.iconBackground,
+                        ),
+                        width: 100,
+                        height: 100,
+                        child: Icon(Icons.speaker_phone,
+                            color: isDark ? dark.iconColor : light.iconColor,
+                            size: 70)),
+
+
+ appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.0),
+        child: AppBar(
+          iconTheme: IconThemeData(
+              color: isDark ? dark.topIconColor : light.topIconColor),
+          backgroundColor: isDark ? darkBgColor : lightBgColor,
+          title: Center(
+            child: Text(
+              'ڈیجی رسائی',
+              style: isDark ? dark.heading1 : light.heading1,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.logout,
+                  color: isDark ? dark.topIconColor : light.topIconColor),
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                    (route) => false);
+              },
+            )
+          ],
+        ),
+      ),
+
+ decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              isDark ? darkBgColor : lightBgColor,
+              isDark ? darkBgColor : lightBgColor,
+            ],
+          ),
+        ),
+
+
+  floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: AvatarGlow(
+        animate: isListening,
+        endRadius: 75,
+        glowColor: isDark
+                                        ? dark.borderColor
+                                        : light.borderColor,
+        child: Container(
+          width: 100,
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 15.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                toggleRecording();
+              },
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  child: Icon(isListening ? Icons.mic : Icons.mic_none,
+                      color: isDark ? dark.iconColor : light.iconColor,
+                      size: 50)),
+              focusColor: isDark ? dark.borderColor : light.borderColor,
+              backgroundColor:
+                  isDark ? dark.iconBackground : light.iconBackground,
+            ),
+          ),
+        ),
+      ),
